94274edd7eaa9ad27939d6d2de9b9e24c1d9e884
Alexandr Shadrin <a_s_d@mail.ru>
2019-10-14 12:45:25 +0300

    Initial commit.

--- numbers.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS numbers;
-CREATE TABLE numbers (
-	value INT COMMENT 'Значение'
-) COMMENT = 'Числа для перемножения';
-
-INSERT INTO numbers (value) VALUES (1), (2), (3), (4), (5);
